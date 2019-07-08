f9e2b9877b9c75d52fcd7ca3d19aea3430a8e558
BrettRD <brettrd@brettrd.com>
2019-07-08 17:30:47 +1000

    adds game mechanic, adds regular status, code cleanup

--- ntag21x_rw_seeed.ino
+++ ntag21x_rw_seeed.ino
@@ -1,5 +1,9 @@
 
-
+/*
+players are issued ten darts each,
+darts are coded to the gun
+if the wrong dart is fired, the gun jams
+*/
 
 #include <SPI.h>
 #include <PN532_SPI.h>
@@ -18,12 +22,6 @@ char* ap_name = "";
 char* ap_pass = "";
 char* broker_url = ""
 */
-char mqtt_host_string[50];
-
-
-ESP8266WiFiMulti wifiMulti;
-MQTTClient mqtt;
-
 const int ss_pin = D0;
 const int mot_a = D2;
 const int mot_b = D1;
@@ -31,8 +29,25 @@ const int interlock_pin = A0;
 
 const float rail_filter_alpha = 0.1;
 const int rail_voltage_min = 300;   //minimum plausible raw adc read from the rail
-
 float rail_voltage_filtered = 0;
+
+
+uint32_t last_loop_timestamp = 0;
+uint32_t timestamp = 0;
+
+uint32_t filter_timestamp = 0;
+
+uint32_t report_period = 1000;
+uint32_t report_time = 0;
+
+
+bool motor_jam = false;   //game mechanic, brake the flywheel
+int motor_power_max = 700;
+int motor_power = 0;
+
+
+ESP8266WiFiMulti wifiMulti;
+MQTTClient mqtt;
 
 
 PN532_SPI intf(SPI, ss_pin);
@@ -42,14 +57,18 @@ PN532 nfc = PN532(intf);
 //uint8_t buf[4];
 uint8_t uid[7];         //store the tag uid
 char uid_hex[20];       //format to hex (only 2*7 + 1 bytes used)
-uint8_t uidLength;
+uint8_t uidLength = 7;
 
 // change this during setup, examples here
 char client_id[30] = "testgun";
 char pubTopic[60] = "nerfgun/testgun/lwt";
-char tagTopic[60] = "nerfgun/testgun/state";
+char tagTopic[60] = "nerfgun/testgun/tag";
+char stateTopic[60] = "nerfgun/testgun/state";
 char subTopic[60] = "nerfgun/testgun/power";
 
+
+
+char mqtt_host_string[50];
 char jsonString[100]; //to store the formatted json
 
 
@@ -76,9 +95,6 @@ void mqtt_cb_onDisconnect()
 
 void mqtt_cb_onData(String topic, String data, bool cont)
 {
-    Serial.println(topic);
-    Serial.println(data);
-    
     if(topic == subTopic)
     {
         int power = data.toInt();
@@ -95,19 +111,64 @@ void mqttConnect()
 }
 
 
-void publishTag(uint8_t* uid, bool interlock, float rail_voltage)
+//returns dst
+char* to_hex(char* dst, uint8_t* src, int n)
 {
-    for(uint8_t i=0; i<7; i++)
+    for(uint8_t i=0; i<n; i++)
     {
-      uid_hex[(2*i)+0] = "0123456789abcdef"[uid[i] >>  4 ];
-      uid_hex[(2*i)+1] = "0123456789abcdef"[uid[i] & 0x0f];
+        dst[(2*i)+0] = "0123456789abcdef"[src[i] >>  4 ];
+        dst[(2*i)+1] = "0123456789abcdef"[src[i] & 0x0f];
     }
-    uid_hex[14] = '\0'; //null terminate
-
-    sprintf(jsonString, "{\"uid\":\"%s\", \"interlock\":%d, \"voltage\":%f}", uid_hex, interlock, rail_voltage);
-
-    mqtt.publish(tagTopic, jsonString, 0, 0);
+    dst[2*n] = '\0';
+    return dst;
 }
+
+
+bool tag_match(char* uid_hex)
+{
+    for(int i=0; i<n_darts; i++)
+    {
+        if(0 == strcmp(dart_list[i].dart_uid, uid_hex))
+        {
+            bool tag_match = (0 == strcmp(dart_list[i].gun_id, client_id));
+            return tag_match;
+        }
+    }
+    //default to false, the gun has a very limited list of darts
+    return false;
+}
+
+
+
+void run_motor(int power, bool jam, bool interlock)
+{
+    //if the spin button is released, clear the software jam and power down
+    //otherwise, spin up to the target speed
+    if(!interlock)
+    {
+        jam = false;
+        motor_power = 0;
+    }
+    else
+    {
+        motor_power = power;
+    }
+
+
+    //apply power to the motor
+    //hit the brakes if the jam flag is set
+    if(jam == false)
+    {
+        analogWrite(mot_a, motor_power);
+        digitalWrite(mot_b, LOW);
+    }
+    else
+    {
+        digitalWrite(mot_a, HIGH);  //brake
+        digitalWrite(mot_b, HIGH);
+    }
+}
+
 
 
 void setup(void)
@@ -116,7 +177,8 @@ void setup(void)
     sprintf(client_id, "nerf%06X", ESP.getChipId());
     sprintf(pubTopic,"nerfgun/%s/lwt", client_id);
     sprintf(subTopic,"nerfgun/%s/power", client_id);
-    sprintf(tagTopic, "nerfgun/%s/state", client_id);
+    sprintf(tagTopic, "nerfgun/%s/tag", client_id);
+    sprintf(stateTopic, "nerfgun/%s/state", client_id);
     sprintf(mqtt_host_string, "mqtt://%s:%s#$s", broker_url, "1883", client_id);
 
     pinMode(mot_a, OUTPUT);
@@ -148,39 +210,60 @@ void setup(void)
     mqttConnect();
 }
 
+
+
+
 void loop(void)
 {
+    last_loop_timestamp = timestamp;
+    timestamp = millis();
+
     bool wifi_online = (wifiMulti.run() == WL_CONNECTED);
 
     int rail_voltage = analogRead(interlock_pin);
     bool interlock = rail_voltage > rail_voltage_min;
     if(interlock)
     {
+        float rail_filter_alpha_t = rail_filter_alpha * (float)(timestamp - filter_timestamp) / 1000.0;
         rail_voltage_filtered = (((float)rail_voltage * rail_filter_alpha) + rail_voltage_filtered) / (1 + rail_filter_alpha);
     }
+    run_motor(motor_power_max, motor_jam, interlock);
 
     if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength))
     {
         if(uidLength == 7)
         {
-
+            to_hex(uid_hex, uid, 7);
             if(wifi_online)
             {
-                publishTag(uid, interlock, rail_voltage);
+                sprintf(jsonString, "{\"uid\":\"%s\"}", uid_hex);
+                mqtt.publish(tagTopic, jsonString, 0, 0);
             }
 
+            motor_jam = !tag_match(uid_hex);
+            run_motor(motor_power_max, motor_jam, interlock);
+/*
             Serial.print("uid = ");
-            for(uint8_t i=0; i<uidLength; i++)
-            {
-                Serial.print("0123456789abcdef"[uid[i] >> 4]);
-                Serial.print("0123456789abcdef"[(uid[i] & 0x0f)]);
-            }
-            Serial.println("");
+            Serial.println(uid_hex);
+*/
         }
     }
 
+    if(timestamp > report_time + report_period)
+    {
+        report_time += report_period;
+        if(wifi_online)
+        {
+            sprintf(jsonString, "{\"interlock\":%d, \"voltage\":%f}", interlock, rail_voltage_filtered);
+            mqtt.publish(stateTopic, jsonString, 0, 0);
+        }
+    }
+
+
     mqtt.handle();
 
+    //Serial.print("MQTT handled: ");
+    //Serial.println(millis());
 
     // if NTAG21x enables r/w protection, uncomment the following line 
     // nfc.ntag21x_auth(password);
